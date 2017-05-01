8261d5fa0e170b5bdc1d60632c768e53a2456bd4
manimutta <mani4654@gmail.com>
2017-05-01 17:25:54 +0530

    userfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :leaves, dependent: :destroy
   validates_presence_of :name, :email
-  validates :remaining_leaves, :numericality => { :less_than_or_equal_to => 14 }
 
 
   def self.from_omniauth(auth)
