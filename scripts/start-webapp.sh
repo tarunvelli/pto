baff3664ac67d9c46d87bea0bb2486fd45cd5fa1
Pradeep G <itsprdp@gmail.com>
2017-06-19 18:55:09 +0530

    Added startup script to avoid passenger worker env problem

---
+++ scripts/start-webapp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+APP_DIR=/pipeline/source/
+bash $APP_DIR/scripts/app_env_vars.sh
+sed -i -e "s@test@$RAILS_ENV@" $APP_DIR/scripts/config/database.yml
+bash $APP_DIR/scripts/nginx_conf.sh
+
+service nginx start
