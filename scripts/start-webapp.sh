b1c85fb05e63e2fda3cb8f8863657591c6820466
Pradeep G <itsprdp@gmail.com>
2017-07-24 13:43:08 +0530

    Fixed whenever command

--- scripts/start-webapp.sh
+++ scripts/start-webapp.sh
@@ -4,6 +4,6 @@ APP_DIR=/pipeline/source/
 bash $APP_DIR/scripts/app_env_vars.sh
 sed -i -e "s@test@$RAILS_ENV@" $APP_DIR/config/database.yml
 bash $APP_DIR/scripts/nginx_conf.sh
-RAILS_ENV=$RAILS_ENV whenever --update-crontab
+cd $APP_DIR && RAILS_ENV=$RAILS_ENV bundle exec whenever --update-crontab
 
 service nginx start
