9cb498b57e2f717fc040ad7c8f220c0b2c10716e
Pradeep G <itsprdp@gmail.com>
2017-07-24 13:17:59 +0530

    Fixed whenever config to use the Rails env while generating the crontab and to use the right rake task with the namespace

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require File.expand_path(File.dirname(__FILE__) + '/../config/environment')
+
+set :environment, Rails.env
+
 every :weekday, at: '9am' do
-  rake 'send_slack_notification'
+  rake 'slack:send_slack_notification'
 end
 
 every :year, at: 'March 31st 11:59pm' do
