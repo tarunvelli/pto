1d58df35b0b7f5dd551131c4e6a8cceaf9cae3a9
Pradeep Gangadharaiah <itsprdp@gmail.com>
2020-02-27 13:08:41 +0530

    run puma on production and rails 5 protect_from_forgery changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
   include SessionsHelper
   before_action :set_paper_trail_whodunnit
 end
